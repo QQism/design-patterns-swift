c6e1d14d062127fce509530ce0b23c55c8451c18
Quang Quach <quangquach@Quangs-MacBook-Pro.local>
2014-06-05 22:36:36 +0700

    Initial Commit

---
+++ DesignPartterns/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DesignPartterns
+//
+//  Created by Quang Quach on 6/5/14.
+//  Copyright (c) 2014 hungrivity. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
